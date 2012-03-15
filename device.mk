f75e12f3d8435af1e3bce92bbdb78d07befd7881
codenamedroid <codenamegit@gmail.com>
2012-03-14 21:55:15 -0500

    add to fix crespo build with new changes to the proprietary files
    Change-Id: I73ffcdcca51b008ac6f7eea8756cb58c4c6c38f6

--- device.mk
+++ device.mk
@@ -51,3 +51,5 @@ $(call inherit-product, device/samsung/crespo/device_base.mk)
 # commonly available
 $(call inherit-product-if-exists, vendor/samsung/crespo/device-vendor.mk)
 
+$(call inherit-product-if-exists, vendor/samsung/crespo/device-vendor-blobs.mk)
+
